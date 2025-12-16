17fbd8e3a82b84a9242e021ce0509871af3126c2
soporteit <soporteit@bexenmedical.com>
2025-12-16 14:04:40 +0100

    Corregir nombre del archivo importado

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import VishingSimulator from './VishingSimulator.jsx'
+import VishingSimulator from './vishing-simulator.jsx'
 
 // Render the app
 ReactDOM.createRoot(document.getElementById('root')).render(
